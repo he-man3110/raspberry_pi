b6b2e79a7c3604c40b0333493d0dd6e9132af9fc
He_Man <49283646+he-man3110@users.noreply.github.com>
2020-10-13 10:03:21 +0530

    Modified example.py

--- example.py
+++ example.py
@@ -1,5 +1,9 @@
 from mq import *
 import sys, time
+import thingspeak
+
+
+channel = thingspeak.Channel( <id_here>, "")
 
 try:
     print("Press CTRL+C to abort.")
@@ -11,7 +15,8 @@ try:
         sys.stdout.write("\033[K")
         sys.stdout.write("LPG: %g ppm, CO: %g ppm, Smoke: %g ppm" % (perc["GAS_LPG"], perc["CO"], perc["SMOKE"]))
         sys.stdout.flush()
-        time.sleep(0.1)
+        channel.update({1:perc["GAS_LPG"], 2:perc["CO"], 3:perc["SMOKE"]})
+        time.sleep(30)
 
 except:
     print("\nAbort by user")
